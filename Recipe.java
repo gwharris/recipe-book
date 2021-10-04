e14268ec6b53b7575332241c6c7b02fe92903151
Javier Saldana <saldanaj27@gmail.com>
2021-10-04 12:50:36 -0400

    created the Main and Recipe class

---
+++ Recipe.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Recipe {
+    String Name;
+    String Description;
+    ArrayList<String> IngredientList;
+    ArrayList<String> CookingSteps;
+    int RecipeID;
+
+    public Recipe(String n, String d, ArrayList<String> i, ArrayList<String> c) {
+        this.Name = n;
+        this.Description = d;
+        this.IngredientList = i;
+        this.CookingSteps = c;
+    }
+}
