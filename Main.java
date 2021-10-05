ac3a7a9c54265bb4fc089a9afe45513bba4d7f2b
Javier Saldana <saldanaj27@gmail.com>
2021-10-05 12:44:12 -0400

    added more to the framework and added a .gitignore file

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+import java.util.*;
+import java.io.*; 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner input = new Scanner(System.in);
+
+        int option;
+
+
+        System.out.println("Enter '1' for Recipe Creation or '2' for Recipe Retrieval");
+        option = input.nextInt();
+
+        if(option == 1) {
+            
+        } else if(option == 2) {
+
+        } else {
+            System.out.println("ERROR....Invalid Option");
+        }
     }
 }
