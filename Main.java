7d3695642b247980cbfc6a1e508eb0ff9c241b9c
junyi-s <60148174+junyi-s@users.noreply.github.com>
2021-10-07 04:18:15 -0400

    Updated Recipe Retrieval fixed errors

--- Main.java
+++ Main.java
@@ -109,28 +109,46 @@ public class Main {
             // two people should work on this. 
             // 1) the search function and the browsing all existing recipes
             // 2) the recipe exploration...on the project 1 pdf
-            int searchType = searchRecipes();
+
+            System.out.println("(1) Would you like to search for a recipe or (2) browse recipes? Enter 1 or 2.");
+            int inputType = input.nextInt();;
+            int invalidFlag = 1;
+            int searchType = 0;
+
+            //Choose search option and make sure there are no invalid inputs
+            while (invalidFlag == 1) {
+                if (inputType == 1) {
+                    searchType = 1;
+                    invalidFlag = 0;
+                } else if (inputType == 2) {
+                    searchType = 2; 
+                    invalidFlag = 0;
+                } else {
+                    System.out.println("That was an invalid option.");
+                }
+            }
 
             //Retrieve Recipes
             String inputSearch;
 
             //Search with name
             if (searchType == 1) {
-                System.out.println("What recipe would you like to look up? Type in below: ")
+                System.out.println("What recipe would you like to look up? Type in below: ");
                 inputSearch = input.nextLine();
                 //********DOES NOT ACCOUNT FOR NUMBERS IN SEARCH********* (will cause an error)
                 String inputSearch_lower = inputSearch.toLowerCase();
-                boolean recipeFound = false;
+                int recipeFound = 0;
 
                 //Checks to make sure recipe is in the list
                 for (int i = 0; i < recipeList.size(); i++) {
-                    String recipeName = recipeList.get(i).name().toLowerCase();
+                    //********Issue retreiving the recipe object*********
+                    String recipeName = recipeList.get(i).Name.toLowerCase();
                     if (inputSearch_lower.equals(recipeName)) {
-                        recipeFound = true;
+                        recipeFound = 1;
                     } 
                 }
 
-                if (recipeFound) {
+                if (recipeFound == 1) {
                     //Implement Recipe Exploration (function would be easier as the other search also needs it)
                 } else {
                     System.out.println("There is no recipe under that search.");
@@ -138,52 +156,29 @@ public class Main {
                 }
 
             //Search by recipe list
-            } else { //searchType == 2
-                searchAll();
+            } else if (searchType == 2){ //searchType == 2
+                System.out.println("Please choose a recipe from the list below.");
+                for (int i = 0; i < recipeList.size(); i++) {
+                    //********Issue retreiving the recipe object*********
+                    String recipeName = Integer.toString(i + 1) + ". " + recipeList.get(i).Name;
+                    System.out.println(recipeName);
+                }
+                System.out.println("Please type the number of the recipe you would like: ");
+                try {
+                    int recipeNum = input.nextInt() - 1;
+                } catch (NumberFormatException n) {
+                    System.out.println("An error occurred.");
+                    //Can implement to loop back to search
+                }
+                //Implement Recipe Exploration use (recipeNum)
+            } else {
+                System.out.println("ERROR....Something went wrong.");
             }
             
             // the second person needs information from the first one but you can still work on it
         } else { // error, end the program
             System.out.println("ERROR....Invalid Option");
         }
-    }
-
-    //Search for the recipe specified by the user. 
-    public int searchRecipes() {
-        System.out.println("(1) Would you like to search for a recipe or (2) browse recipes? Enter 1 or 2.");
-        String inputType = input.nextLine();
-        int invalidFlag = 1;
-        int searchType;
-
-        //Choose search option and make sure there are no invalid inputs
-        while (invalidFlag == 1) {
-            if (inputType == 1) {
-                searchType = 1;
-                invalidFlag = 0;
-            } else if (inputType == 2) {
-                searchType = 2; 
-                invalidFlag = 0;
-            } else {
-                System.out.println("That was an invalid option. Please try again.");
-            }
-        }
-        return searchType;
-    }
-    //Prints out the list of recipes and allows the user to choose one to view
-    public void searchAll() {
-        System.out.println("Please choose a recipe from the list below.");
-                for (int i = 0; i < recipeList.size(); i++) {
-                    String recipeName = Integer.tostring(i + 1) + ". " + recipeList.get(i).name();
-                    System.out.println(recipeName);
-                }
-                System.out.println("Please type the number of the recipe you would like: ")
-                try {
-                   int recipeNum = Integer.parseInt(input.nextLine()) - 1; 
-                } catch (NumberFormatException n) {
-                    System.out.println("An error occurred.");
-                    //Can implement to loop back to search
-                }
-                //Implement Recipe Exploration use (recipeNum)
     }
 }
 
