4b87b0c5a3eab45af58753c5597842ac58c824a6
instagr4h4m <grahamh1019@gmail.com>
2021-10-07 12:51:18 -0400

    Add comments

--- Main.java
+++ Main.java
@@ -67,9 +67,9 @@ public class Main {
             userInput = input.nextLine();
             flag = isNumber(userInput);
         }
-        
-        // Validate input
         double option = Double.parseDouble(userInput);
+
+        // Process user's choice
         if(option == 0) System.out.println("\nThanks for using the recipe book! \nProgram quitting.\n"); // Quit
 
         else if(option == 1) { // Recipe Creation
